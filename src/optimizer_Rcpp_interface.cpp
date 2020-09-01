d76ba4df87ccf08852c0ad6448526e1a4723fe74
Konrad Krämer <Konrad_kraemer@yahoo.de>
2020-09-01 09:09:21 +0200

    changed convergence check from 200 to 900 generations

--- src/optimizer_Rcpp_interface.cpp
+++ src/optimizer_Rcpp_interface.cpp
@@ -434,8 +434,8 @@ double Optimizer_Rcpp_interface::pso() { // (labled with ! need check)
       convergence_check = convergence_check + 1;
       convergence_check_stop = convergence_check_stop + 1;
     }
-    if(convergence_check_stop > 200) {
-      Rcpp::Rcerr << "No convergence for 200 generations. Optimizing stoped" << std::endl;
+    if(convergence_check_stop > 900) { // 200
+      Rcpp::Rcerr << "No convergence for 900 generations. Optimizing stoped" << std::endl;
       break;
     }
     Rcpp::Rcerr << "global best val" << "\t" << global_best_val << std::endl;
