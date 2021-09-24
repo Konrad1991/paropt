89236cb96e6efd4a8e98586185e340a79d533657
Konrad <35558461+Konrad1991@users.noreply.github.com>
2021-09-24 10:58:01 +0200

    Update optimizer_new.cpp

--- src/optimizer_new.cpp
+++ src/optimizer_new.cpp
@@ -4,14 +4,40 @@ typedef int (*OS)(double &t, std::vector<double> &params, std::vector<double> &s
 typedef double (*fctptr)(std::vector<double> &param_combi_start, OS ode_system, time_state_information_Rcpp_interface &solv_param_struc);
 
 
+void generate_random_int(Rcpp::IntegerVector& inp, std::vector<int>& res) {
+
+  double temp;
+  for(int i = 0; i < res.size(); i++) {
+    GetRNGstate();
+    temp =  R::runif(0, 1);
+    PutRNGstate();
+    res[i] = floor(temp*inp.size());
+  }
+}
+
+int generate_random_int(Rcpp::IntegerVector& inp) {
+  GetRNGstate();
+  double temp = R::runif(0, 1);
+  PutRNGstate();
+   return floor(temp*inp.size());
+}
+
+void generate_random_double(Rcpp::NumericVector& inp) {
+  for(int i = 0; i < inp.size(); i++) {
+    GetRNGstate();
+    inp[i] = R::runif(0, 1);
+    PutRNGstate();
+  }
+}
+
 struct SwarmStruct {
-  Rcpp::NumericMatrix S;
-  Rcpp::NumericMatrix velocities;
-  Rcpp::NumericVector best_errors;
-  Rcpp::NumericVector current_errors;
+  Rcpp::NumericMatrix S; // shuffle!
+  Rcpp::NumericMatrix velocities; // shuffle!
+  Rcpp::NumericVector best_errors; // shuffle!
+  Rcpp::NumericVector current_errors; // shuffle!
   int best_particle;
   Rcpp::NumericVector parameter_of_best_particle;
-  Rcpp::NumericMatrix personal_best_parameters;
+  Rcpp::NumericMatrix personal_best_parameters; // shuffle!
   double global_best_error;
   int n_swarm;
   int n_params;
@@ -19,6 +45,40 @@ struct SwarmStruct {
   time_state_information_Rcpp_interface model;
   OS userode;
 };
+
+void shuffle(SwarmStruct& inp) {
+  int indx1;
+  int indx2;
+
+  double swap1;
+  Rcpp::NumericVector swap2(inp.n_params);
+
+  Rcpp::IntegerVector range(inp.n_swarm);
+  for(int i = 0; i < range.size(); i++) {
+    range[i] = i;
+  }
+
+  for(int i = 0; i < 1000; i++) { // 10000 better
+    indx1 = arma::randi<int>(arma::distr_param(0, inp.n_swarm-1)); //generate_random_int(range);
+    indx2 = arma::randi<int>(arma::distr_param(0, inp.n_swarm-1)); //generate_random_int(range);
+    swap2 = inp.S(indx1, Rcpp::_);
+    inp.S(indx1, Rcpp::_) = inp.S(indx2, Rcpp::_);
+    inp.S(indx2, Rcpp::_) = swap2;
+    swap2 = inp.velocities(indx1, Rcpp::_);
+    inp.velocities(indx1, Rcpp::_) = inp.velocities(indx2, Rcpp::_);
+    inp.velocities(indx2, Rcpp::_) = swap2;
+    swap2 = inp.personal_best_parameters(indx1, Rcpp::_);
+    inp.personal_best_parameters(indx1, Rcpp::_) = inp.personal_best_parameters(indx2, Rcpp::_);
+    inp.personal_best_parameters(indx2, Rcpp::_) = swap2;
+
+    swap1 = inp.best_errors[indx1];
+    inp.best_errors[indx1] = inp.best_errors[indx2];
+    inp.best_errors[indx2] = swap1;
+    swap1 = inp.current_errors[indx1];
+    inp.current_errors[indx1] = inp.current_errors[indx2];
+    inp.current_errors[indx2] = swap1;
+  }
+}
 
 void calculate_errors_init(SwarmStruct& inp) {
   using namespace Rcpp;
@@ -47,7 +107,6 @@ void random_num_vec(Rcpp::NumericVector& inp) {
 void init_fct(SwarmStruct& sw, int n_swarm,  Rcpp::NumericVector& lb, Rcpp::NumericVector& ub, OS ode_system, fctptr solver,
               time_state_information_Rcpp_interface& solv_param_struc) {
 
-  Rcpp::RNGScope scope;
   sw.n_swarm = n_swarm;
   sw.n_params = lb.size();
   sw.S = Rcpp::NumericMatrix(n_swarm, lb.size());
@@ -87,26 +146,6 @@ struct neighbour {
   std::vector<int> K;
 };
 
-
-void generate_random_int(Rcpp::IntegerVector& inp, std::vector<int>& res) {
-
-  Rcpp::Vector<INTSXP> temp(1);
-  for(int i = 0; i < res.size(); i++) {
-    GetRNGstate();
-    temp =  Rcpp::sample(inp, 1);
-    PutRNGstate();
-    res[i] = temp[0];
-  }
-}
-
-int generate_random_int(Rcpp::IntegerVector& inp) {
-  GetRNGstate();
-  Rcpp::Vector<INTSXP> temp = Rcpp::sample(inp, 1);
-  PutRNGstate();
-   return temp[0];
-}
-
-
 void calc_neighbours(neighbour& NE, int num_particle) {
   Rcpp::IntegerVector K = {0, 1, 2, 3};
 
@@ -130,7 +169,7 @@ void calc_neighbours(neighbour& NE, int num_particle) {
 
 
   for(int i = 0; i < num_particle; i++) {
-    NE.K[i] = generate_random_int(K);
+    NE.K[i] = arma::randi<int>(arma::distr_param(0,3)); //generate_random_int(K);
 
     if(NE.K[i] == 0) {
       K0.neigh[0] = i;
@@ -138,6 +177,7 @@ void calc_neighbours(neighbour& NE, int num_particle) {
       NE.N[i].neigh = K0.neigh;
     } else if(NE.K[i] == 1) {
       generate_random_int(range, K1.neigh);
+      K1.neigh = informants;
       NE.N[i].neigh.resize(2);
       NE.N[i].neigh = K1.neigh;
     } else if(NE.K[i] == 2) {
@@ -161,7 +201,7 @@ void check_boundaries(SwarmStruct& inp, int n_params,
     for(int i = 0; i < n_params; i++) {
       if(inp.S(index, i) > ub[i]) {
         inp.S(index, i) = ub[i];
-      } else if(inp.S(index, i) < ub[i]) {
+      } else if(inp.S(index, i) < lb[i]) {
         inp.S(index, i) = lb[i];
       }
     }
@@ -191,7 +231,7 @@ OS t_odes)
   OS ode_system = t_odes;
   time_state_information_Rcpp_interface model = t_model;
 
-  bool convergence = false;
+  int convergence_check = 0;
 
   Rcpp::NumericVector local_best_errors(n_swarm);
   neighbour hood;
@@ -199,6 +239,7 @@ OS t_odes)
   SwarmStruct SW;
 
   init_fct(SW, n_swarm, lb, ub, ode_system, solver, model);
+
   int position_local_best;
   int position_global_best;
   double global_best;
@@ -219,64 +260,78 @@ OS t_odes)
   int i = 0;
   RcppThread::ThreadPool pool;
   std::vector<std::future<double> > futures(n_swarm);
-  std::vector<std::vector<double> > temp(n_swarm);
-  for(int i = 0; i < n_swarm; i++) {
-    temp[i].resize(n_params);
-  }
+  std::vector<double> temp(n_params);
+
 
   while(i < n_generations) {
 
-    calc_neighbours(hood, n_swarm);
+    if(i == 0 || convergence_check >= 1) {
+      shuffle(SW);
+      calc_neighbours(hood, n_swarm);
+      convergence_check = 0;
+    }
 
-    par_w = par_w_max - i*(par_w_max - par_w_min)/n_generations;
+    par_w = par_w_max - static_cast<double>(i)*(par_w_max - par_w_min)/static_cast<double>(n_generations);
     cog = initial_cog - (initial_cog - final_cog) * (i + 1) / n_generations;
     soc = initial_soc - (initial_soc - final_soc) * (i + 1) / n_generations;
+    double v = 0.9;
+    double chi;
+    chi = 2*v;
+    GetRNGstate();
+    double omega = cog*R::runif(0, 1) + soc*R::runif(0,1);
+    if(omega < 4.) {
+      omega = 4.;
+    }
+    PutRNGstate();
+    chi = chi/std::abs(2. - omega - std::sqrt(omega*(omega -4.) ));
 
     for(int j = 0; j < n_swarm; j++) {
 
       current_erros_of_hood.resize(hood.K[j]);
 
-
       for(int k = 0; k < current_erros_of_hood.size(); k++) {
         current_erros_of_hood[k] = SW.best_errors[hood.N[j].neigh[k]];
       }
-       position_local_best = std::min_element(current_erros_of_hood.begin(),
+
+      position_local_best = std::min_element(current_erros_of_hood.begin(),
                                     current_erros_of_hood.end()) - current_erros_of_hood.begin();
 
-      std::cout << hood.K[position_local_best] << std::endl;
-      local_best_parameters = SW.S(hood.K[position_local_best], Rcpp::_);
+      local_best_parameters = SW.personal_best_parameters(hood.N[j].neigh[position_local_best], Rcpp::_);
 
-      GetRNGstate();
-      rand1 = Rcpp::runif(n_params)*cog;
-      rand2 = Rcpp::runif(n_params)*soc;
-      PutRNGstate();
+      generate_random_double(rand1);
+      generate_random_double(rand2);
 
       SW.velocities(j, Rcpp::_) = par_w*SW.velocities(j, Rcpp::_) +
-                    rand1*(SW.personal_best_parameters(j, Rcpp::_) - SW.S(j, Rcpp::_)) +
-                    rand2*(SW.S(position_local_best, Rcpp::_) - SW.S(j, Rcpp::_));
+                    cog*rand1*(SW.personal_best_parameters(j, Rcpp::_) - SW.S(j, Rcpp::_) ) +
+                    soc*rand2*(local_best_parameters - SW.S(j, Rcpp::_) );
 
+      SW.velocities(j, Rcpp::_) = SW.velocities(j, Rcpp::_)*chi;
       SW.S(j, Rcpp::_) = SW.S(j, Rcpp::_) + SW.velocities(j, Rcpp::_);
 
       check_boundaries(SW, n_params, lb, ub, j);
+  }
 
+  for(int j = 0; j < n_swarm; j++) {
       for(int k = 0; k < n_params; k++) {
-        temp[j][k] = SW.S(j, k);
+        temp[k] = SW.S(j, k);
       }
 
-      futures[j] = pool.pushReturn(SW.lossfct, std::ref(temp[j]), SW.userode, std::ref(SW.model) );
+      futures[j] = pool.pushReturn(SW.lossfct, std::ref(temp), SW.userode, std::ref(SW.model) );
 
       SW.current_errors[j] = futures[j].get();
+  }
 
+  for(int j = 0; j < n_swarm; j++) {
       if(SW.current_errors[j] < SW.best_errors[j]) {
         SW.best_errors[j] = SW.current_errors[j];
         SW.personal_best_parameters(j, Rcpp::_) = SW.S(j, Rcpp::_);
       }
+  }
 
-    }
 
     position_global_best = std::min_element(SW.best_errors.begin(),
                                  SW.best_errors.end()) - SW.best_errors.begin();
-    global_best = SW.best_errors(position_global_best);
+    global_best = SW.best_errors[position_global_best];
 
     if(global_best < SW.global_best_error) {
       SW.global_best_error = global_best;
@@ -285,14 +340,21 @@ OS t_odes)
       if(SW.global_best_error <= desired_error) {
         break;
       }
+    } else {
+      convergence_check++;
     }
 
     i++;
 
-    Rcpp::Rcout << "generation is: " << i << "\t" << "global best error is:  " << SW.global_best_error << std::endl;
+    if(i % 50 == 0) {
+        Rcpp::Rcout << "generation is: " << i << "\t" << "global best error is:  " << SW.global_best_error << std::endl;
+    }
+
 
     Rcpp::checkUserInterrupt();
   }
+
+  Rcpp::Rcout << SW.parameter_of_best_particle << std::endl;
 
   return Rcpp::List::create(Rcpp::Named("global best error") = SW.global_best_error,
                             Rcpp::Named("best parameters") = SW.parameter_of_best_particle);;
