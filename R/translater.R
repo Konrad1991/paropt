f39c5ff2157e3805e982e33e2078383ebb5bc316
Konrad <Konrad_kraemer@yahoo.de>
2022-07-19 16:27:37 +0200

    Update translater.R

--- R/translater.R
+++ R/translater.R
@@ -3,17 +3,25 @@ MA2 <- R6::R6Class("MA2",
 
     public = list(
 
-      build2 = function(verbose = FALSE, reference = FALSE) {
+      build2 = function(verbose = FALSE, reference = FALSE, name_f, args_f) {
         self$getast()
         self$ast2call()
         self$call2char()
 
         exporter_signature = "SEXP getXPtr();"
-        signature = "void ode(double t, double* params, int size_params, double* y_, double* ydot_, int NEQ) {"
-        pointer2sexp = "sexp parameter(size_params, params, 2); sexp y(NEQ, y_, 2); sexp ydot(NEQ, ydot_, 2);"
-        exporter = "SEXP getXPtr() {
-                    typedef void (*funcPtr)(double t, double* params, int size_params, double* y_, double* ydot_, int NEQ);
-                    return XPtr<funcPtr>(new funcPtr(& ode )); }"
+        signature = paste("void",
+                          name_f,
+                          "(double t, double* params, int size_params, double* y_, double* ydot_, int NEQ) {")
+        pointer2sexp = paste("sexp",
+                             args_f[2],
+                             "(size_params, params, 2);",
+                             "sexp",
+                             args_f[3], "(NEQ, y_, 2);",
+                             "sexp",
+                             args_f[4], "(NEQ, ydot_, 2);")
+        exporter = paste("SEXP getXPtr() {
+                    typedef void (*funcPtr)(double t, double* params, int size_params, double* y_, double* ydot_, int NEQ);",
+                    "return XPtr<funcPtr>(new funcPtr(&", name_f, ")); }")
 
         fct = c(
           exporter_signature,
@@ -45,13 +53,19 @@ MA2 <- R6::R6Class("MA2",
 #' @param verbose If set to TRUE the output of RcppXPtrUtils::cppXPtr is printed.
 convert <- function(f, verbose = FALSE) {
 
+    stopifnot(!missing(f))
     stopifnot(is.function(f))
     stopifnot(is.logical(verbose))
+    
+    # get name, args etc. check whether correct
+    name_f <- as.character(substitute(f))
+    args_f <- methods::formalArgs(f)
+    stopifnot(length(args_f) == 4)
 
     desired_type = 'sexp'
 
     a = MA2$new(f, desired_type)
-    fct <- a$build2(verbose, reference = TRUE)
+    fct <- a$build2(verbose, reference = TRUE, name_f, args_f)
 
     fct_ret = NULL
 
